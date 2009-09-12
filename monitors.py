1fa12feedbd9fcd7578f2d58c78419c2d137b29d
Gavin M. Roy <gmr@Gavin-M-Roys-Office-iMac.local>
2009-09-11 20:31:12 -0400

    Todays work

--- monitors.py
+++ monitors.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-monitors.py
+Monitoring options to get queue depths for auto-ramp-up and ramp-back of consumers
+
+Should other alternatives to Alice present themselves, this file is where they
+should go.
 
 Created by Gavin M. Roy on 2009-09-10.
 Copyright (c) 2009 Insider Guides, Inc. All rights reserved.
 """
 
+import json
 import logging
 import sys
 import os
@@ -18,10 +22,21 @@ class alice:
         logging.debug('Alice Monitor created')
         pass
         
-    def getQueueDepth(self, host = 'localhost', queue = 'test' ):
-        url = 'http://%s:9999/queues' % host
+    def getQueueDepth(self, host = 'localhost', queue_name = 'test' ):
+        
+        # Get the queue data by passing in various flags
+        url = 'http://%s:8161/queues/root/name/consumers/messages/messages_ready' % host
         logging.debug('Querying %s' % url)
-        pass
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+        
+        # Loop through the queues and try and find the one we're looking for
+        for queue in data['queues']:
+            if queue['name'] == queue_name:
+                return {'consumers': queue['consumers'], 'depth': queue['messages'] }
+
+        # We didn't find the queue in the list, so return 0's
+        return { 'consumers': 0, 'depth': 0}
 
 class aliceTests(unittest.TestCase):
     def setUp(self):
@@ -29,4 +44,7 @@ class aliceTests(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    
+    a = alice()
+    print a.getQueueDepth('mq07', 'Hotmail')
--- rejected.py
+++ rejected.py
@@ -4,9 +4,8 @@ Rejected AMQP Consumer Framework
 
 A multi-threaded consumer application and how!
 
-Copyright 2009 Insider Guides, Inc.
-
-2009-06-09 Gavin M. Roy <gmr@myyearbook.com>
+Created by Gavin M. Roy on 2009-09-10.
+Copyright (c) 2009 Insider Guides, Inc.. All rights reserved.
 """
 
 import amqplib.client_0_8 as amqp
@@ -19,18 +18,271 @@ import threading
 import time
 import yaml
 
-import mcp
+from monitors import alice
 
 # Number of seconds to sleep between polls
 mcp_poll_delay = 5
 
 version = '0.1'
+
+class consumerThread( threading.Thread ):
+    """ Consumer Class, Handles the actual AMQP work """
+    
+    def __init__( self, configuration, thread_name, binding_name, connect_name ):
+
+        logging.debug('Initializing a Consumer class in thread "%s"' % thread_name)
+
+        # Rejected full Configuration
+        self.config = configuration
+
+        # Variables held for future use
+        self.binding_name = binding_name
+        self.connect_name = connect_name
+        self.running = True
+        self.thread_name = thread_name
+        self.messages_processed = 0
+
+        # Init the Thread Object itself
+        threading.Thread.__init__(self)  
+
+    def connect( self, configuration ):
+        """ Connect to an AMQP Broker  """
+
+        logging.debug('Creating a new connection for "%s" in thread "%s"' % ( self.binding_name, self.thread_name ) )
+
+        return amqp.Connection( host ='%s:%s' % ( configuration['host'], configuration['port'] ),
+                                userid = configuration['user'], 
+                                password = configuration['pass'], 
+                                ssl = configuration['ssl'],
+                                virtual_host = configuration['vhost'] )
+
+    def getInformation(self):
+        """ Grab Information from the Thread """
+
+        return { 
+                 'connection': self.connect_name, 
+                 'binding': self.binding_name,
+                 'queue': self.queue_name,
+                 'processed': self.messages_processed
+               }
+
+    def run( self ):
+        """ Meat of the queue consumer code """
+
+        logging.debug( 'In consumer run for thread "%s"' % self.thread_name )
+
+        # Connect to the AMQP Broker
+        self.connection = self.connect( self.config['Connections'][self.connect_name] )
         
+        # Create the Channel
+        self.channel = self.connection.channel()
+
+        # Create / Connect to the Queue
+        self.queue_name = self.config['Bindings'][self.binding_name]['queue']
+        queue_auto_delete = self.config['Queues'][self.queue_name ]['auto_delete']
+        queue_durable = self.config['Queues'][self.queue_name ]['durable']
+        queue_exclusive = self.config['Queues'][self.queue_name ]['exclusive']
+
+        self.channel.queue_declare( queue = self.queue_name , 
+                                    durable = queue_durable,
+                                    exclusive = queue_exclusive, 
+                                    auto_delete = queue_auto_delete )
+
+        # Create / Connect to the Exchange
+        exchange = self.config['Bindings'][self.binding_name]['exchange']
+        exchange_auto_delete = self.config['Exchanges'][exchange]['auto_delete']
+        exchange_durable = self.config['Exchanges'][exchange]['durable']
+        exchange_type = self.config['Exchanges'][exchange]['type']
+        
+        self.channel.exchange_declare( exchange = exchange, 
+                                       type = exchange_type, 
+                                       durable = exchange_durable,
+                                       auto_delete = exchange_auto_delete)
+
+        # Bind to the Queue / Exchange
+        self.channel.queue_bind( queue = self.queue_name, 
+                                 exchange = exchange,
+                                 routing_key = self.binding_name )
+
+        # Let the consuming start
+        self.consumer_tag = self.channel.basic_consume( 
+                                queue = self.queue_name, 
+                                no_ack = self.config['Bindings'][self.binding_name]['no_ack'],
+                                callback = self.receive )
+
+        # Set our prefetch QOS
+        self.channel.basic_qos( 0, 0, False )
+
+        # Wait for messages
+        logging.debug( 'Waiting on messages for "%s"' %  self.thread_name )
+
+        # Loop as long as the thread is running
+        while self.running == True:
+            try:
+                self.channel.wait()
+            except AttributeError:
+                break
+            except IOError:
+                break
+                
+    def receive(self, message):
+        """ Receive a message """
+        
+        if message is not None:
+            #logging.debug('Received a message on "%s".' % self.thread_name)
+            #print message.body
+            self.channel.basic_ack(message.delivery_tag)
+            self.messages_processed += 1
+        
+    def shutdown(self):
+        """ Gracefully close the connection """
+        
+        logging.debug('Shutting down consumer "%s"' % self.thread_name )
+        
+        # Let the loop know we're not looping
+        self.running = False
+        try:
+            self.connection.close()
+        except IOError:
+            pass
+    
+class mcp:
+    """ Master Control Process keeps track of threads and threading needs """
+
+    def __init__(self, config, options):
+        logging.debug('MCP Created')
+        self.alice = alice()
+        self.bindings = []
+        self.config = config
+        pass
+
+    def poll(self):
+        logging.debug('MCP Polling')
+        for binding in self.bindings:
+            new_threads = {}
+            for thread in binding['threads']:
+                info = binding['threads'][thread].getInformation()
+                data = self.alice.getQueueDepth(info['connection'], info['queue'])
+                
+                # Easier to work with variables
+                queue_depth = int(data['depth'])
+                min = self.config['Bindings'][info['binding']]['consumers']['min']
+                max = self.config['Bindings'][info['binding']]['consumers']['max']
+                threshold = self.config['Bindings'][info['binding']]['consumers']['threshold']
+
+                if queue_depth > threshold and binding['threadCount'] < max:
+                    
+                    logging.info( 'Spawning worker thread for connection "%s" binding "%s": %i messages pending, %i threshhold, %i min, %i max, %i consumers active.' % 
+                                    ( info['connection'], 
+                                      info['binding'], 
+                                      queue_depth, 
+                                      threshold,
+                                      min,
+                                      max,
+                                      binding['threadCount'] ) )
+
+                    # Create a unique thread name
+                    thread_name = '%s_%s_%i' % ( info['connection'], 
+                                                 info['binding'], 
+                                                 binding['threadCount'])
+
+                    # Create the new thread making it use self.consume
+                    thread = consumerThread( self.config,
+                                             thread_name, 
+                                             info['binding'], 
+                                             info['connection'] );
+
+                    # Add to our dictionary of active threads
+                    new_threads[thread_name] = thread
+                    
+                    # Increment our thread counter
+                    binding['threadCount']  += 1
+
+                    # Set the name of the thread for future use
+                    thread.setName(thread_name)
+
+                    # Start the thread
+                    thread.start()
+                    
+                    # We only want 1 new thread at a time
+                    break
+
+            # Append any new threads
+            if len(new_threads) > 0:
+                binding['threads'].update(new_threads)
+
+            # Check if our queue depth is below our threshold and we have more than the min amount
+            if queue_depth < threshold and binding['threadCount'] > min:
+
+                # Remove a thread
+                for thread in binding['threads']:
+                    logging.info( 'Removed worker thread for connection "%s" binding "%s": %i messages pending, %i threshhold, %i min, %i max, %i consumers active.' % 
+                                    ( info['connection'], 
+                                      info['binding'], 
+                                      queue_depth, 
+                                      threshold,
+                                      min,
+                                      max,
+                                      binding['threadCount'] ) )
+                                      
+                    binding['threads'][thread].shutdown()
+                    binding['threadCount']  -= 1
+                    del_thread = True
+                    break;
+
+                # Remove it from our thread dictionary
+                del binding['threads'][thread] 
+
+    def shutdown(self):
+        logging.debug('MCP Shutting Down')
+        for binding in self.bindings:
+            for thread in binding['threads']:
+                binding['threads'][thread].shutdown()
+
+    def start(self):
+        logging.debug('MCP Starting Up')
+
+        for binding_name in self.config['Bindings']:
+
+            binding = { 'name': binding_name }
+            binding['queue'] = self.config['Bindings'][binding_name]['queue']
+            binding['threads'] = {}
+            binding['threadCount'] = 0
+
+            # For each connection, kick off the min consumers and start consuming
+            for connect_name in self.config['Bindings'][binding_name]['connections']:
+                for i in range( 0, self.config['Bindings'][binding_name]['consumers']['min'] ):
+                    logging.debug( 'Creating worker thread #%i for connection "%s" binding "%s"' % ( i, connect_name, binding_name ) )
+
+                    # Create a unique thread name
+                    thread_name = '%s_%s_%i' % ( connect_name, binding_name, i )
+
+                    # Create the new thread making it use self.consume
+                    thread = consumerThread( self.config,
+                                             thread_name, 
+                                             binding_name, 
+                                             connect_name );
+
+                    # Add to our dictionary of active threads
+                    binding['threads'][thread_name] = thread
+                    
+                    binding['threadCount'] += 1
+
+                    # Set the name of the thread for future use
+                    thread.setName(thread_name)
+
+                    # Start the thread
+                    thread.start()
+
+            # Append this binding to our binding stack
+            self.bindings.append(binding)
+
 def shutdown(signum = 0, frame = None):
     global mcp
     
     logging.info('Graceful shutdown initiated.')
     mcp.shutdown()
+    logging.debug('Graceful shutdown complete')
     os._exit(signum)
 
 def main():
@@ -182,7 +434,7 @@ def main():
     signal.signal(signal.SIGTERM, shutdown)
 
     # Start the Master Control Program ;-)
-    mcp = mcp.mcp(config, options)
+    mcp = mcp(config, options)
     
     # Kick off our core connections
     mcp.start()

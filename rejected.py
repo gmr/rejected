b58de900ac076b3678dae7ec3a842baeb55241df
Gavin M. Roy <gmr@myyearbook.com>
2009-10-08 23:05:34 -0400

    Trying to merge in mixedpuppys changeset since github seemed to break when I tried.  Changeset e650cf7494b0fccfc7e0eb6f1615a79bed70f3cc

--- rejected.py
+++ rejected.py
@@ -91,8 +91,8 @@ class ConsumerThread( threading.Thread ):
             return connection
 
         # amqp lib is only raising a generic exception which is odd since it has a AMQPConnectionException class
-        except Exception as (errno, strerror):
-            logging.error( 'Connection error #%i: %s' % (errno, strerror) )
+        except IOError, e:
+            logging.error( 'Connection error #%i: %s' % (e.errno, e.message) )
             raise ConnectionException
 
     def get_information(self):
@@ -275,18 +275,20 @@ class ConsumerThread( threading.Thread ):
             self.running = False
         
         """ 
-        This is hanging for me at times, non-predictably, I wonder if this has to
-        with the bug in 1.6.0 that was fixed in 1.7.0
-        http://lists.rabbitmq.com/pipermail/rabbitmq-discuss/attachments/20091007/3aaed239/attachment.txt
-         - remove channel closing timeout since it can cause a protocol
-          violation
-        Ideally we'd add this back at some point
+        This hangs because channel.wait in the thread is blocking on socket.recv.
+        channel.close sends the close message, then enters ultimately into
+        socket.recv to get the close_ok response.  Depending on the timing,
+        the channel.wait has picked up the close_ok after channel.close (on main
+        thread) entered socket.recv.
+        
+        I was looking at a nonblocking method to deal with this properly:
+        http://www.lshift.net/blog/2009/02/18/evserver-part2-rabbit-and-comet
         """
         #self.channel.close()
         if self.connection:
             try:
                 self.connection.close()
-            except IOError:
+            except IOError, e:
                 # We're already closed
                 pass
 
@@ -351,7 +353,7 @@ class MasterControlProgram:
             for thread_name, thread in binding['threads'].items():
             
                 # Make sure the thread is still alive, otherwise remove it and move on
-                if not thread.is_alive():
+                if not thread.isAlive():
                     logging.error( 'Encountered a dead thread: %s, removing it from the stack' % thread_name )
                     del binding['threads'][thread_name]
                     continue
@@ -561,7 +563,7 @@ class MasterControlProgram:
                     thread.start()
 
                     # Check to see if the thread is alive before adding it to our stack
-                    if thread.is_alive():
+                    if thread.isAlive():
 
                         # Add to our dictionary of active threads
                         binding['threads'][thread_name] = thread
@@ -618,9 +620,9 @@ def main():
                         configuration data such as the user credentials and \
                         exchange will be derived from the configuration file.")     
 
-    parser.add_option("-f", "--foreground",
-                        action="store_true", dest="foreground", default=False,
-                        help="Do not fork and stay in foreground")                                                                                                                                 
+    parser.add_option("-d", "--detached",
+                        action="store_true", dest="detached", default=False,
+                        help="Run in daemon mode")                                                                                                                                 
 
     parser.add_option("-m", "--monitor",
                         action="store_true", dest="monitor", 
@@ -700,7 +702,7 @@ def main():
         config['Monitor']['enabled'] = True
 
     # Fork our process to detach if not told to stay in foreground
-    if not options.foreground:
+    if options.detached:
         try:
             pid = os.fork()
             if pid > 0:
@@ -751,12 +753,13 @@ def main():
     mcp.start()
     
     # Loop until someone wants us to stop
+    do_poll = options.monitor and not options.single_thread
     while 1:
         
         # Have the Master Control Process poll
         try:
             # Check to see if we need to adjust our threads
-            if options.single_thread is not True:
+            if do_poll:
                 mcp.poll()
 
             # Sleep is so much more CPU friendly than pass

b249e3004141c7dcdcf8c1c1ab3a792de9d48581
Brian K. Jones <bkjones@gmail.com>
2010-10-01 10:56:41 -0400

    First shot, completely untested 'Rabbit' class in monitors.py. Should be close.

--- rejected/monitors.py
+++ rejected/monitors.py
@@ -18,14 +18,14 @@ import time
 import urllib
 
 class Alice:
-    """ 
-    Alice is an interface to the awesome Alice service for RabbitMQ 
+    """
+    Alice is an interface to the awesome Alice service for RabbitMQ
     http://github.com/auser/alice/
     """
     def __init__(self):
         logging.debug('Alice: Monitor created')
         self.cache = {}
-        
+
     def get_queue_depth(self, host = 'localhost', port = '9999', queue_name = 'test' ):
 
         # If port is coming in as None, use the default
@@ -37,26 +37,68 @@ class Alice:
         if cache_name in self.cache:
             if time.time() - self.cache[cache_name]['timestamp'] < 10:
                 logging.debug( 'Alice: Returning cached values for "%s": consumers: %i, depth: %i' %
-                               ( cache_name, 
-                                 self.cache[cache_name]['consumers'], 
-                                 self.cache[cache_name]['depth'] ) ) 
+                               ( cache_name,
+                                 self.cache[cache_name]['consumers'],
+                                 self.cache[cache_name]['depth'] ) )
                 return self.cache[cache_name]
-        
+
         # Get the queue data by passing in various flags
         url = 'http://%s:%s/queues/root/name/consumers/messages/messages_ready' % ( host, port )
         logging.debug('Alice: Querying %s' % url)
         response = urllib.urlopen(url)
         data = json.loads(response.read())
-        
+
         # Loop through the queues and try and find the one we're looking for
         for queue in data['queues']:
             if queue['name'] == queue_name:
                 self.cache[cache_name] = {'timestamp': time.time(), 'consumers': int(queue['consumers']), 'depth': int(queue['messages']) }
-                logging.debug('Alice: Caching and returning values for "%s": consumers: %i, depth: %i' % 
-                               ( cache_name, 
-                                 self.cache[cache_name]['consumers'], 
+                logging.debug('Alice: Caching and returning values for "%s": consumers: %i, depth: %i' %
+                               ( cache_name,
+                                 self.cache[cache_name]['consumers'],
                                  self.cache[cache_name]['depth'] ) )
                 return self.cache[cache_name]
 
         # We didn't find the queue in the list, so return 0's
         return { 'consumers': 0, 'depth': 0}
+
+class Rabbit(object):
+    def __init__(self):
+        logging.debug("Rabbit: Monitor created")
+        self.cache = {}
+
+    def check_cache(self, cache_name):
+        if cache_name in self.cache:
+            if time.time() - self.cache[cache_name]['timestamp'] < 10:
+                logging.debug('Rabbit: Returning cached values for "%s": consumers: %i, depth: %i' %
+                              ( cache_name,
+                                self.cache[cache_name]['consumers'],
+                                self.cache[cache_name]['depth']))
+                return self.cache[cache_name]
+            else:
+                return None
+        else:
+            return None
+
+    def get_queue_depth(self, host = 'localhost', port = '55672', vhost = '%2f', queue_name = 'test'):
+        cache_name = '%s-%s' % (host, queue_name)
+        cache_val = self.check_cache(cache_name)
+        if cache_val is not None:
+            return  cache_val
+
+        url = 'http://%s:%s/api/queues/%s/%s' % (host, port, vhost, queue_name)
+        logging.debug('Rabbit: Querying %s' % url)
+        try:
+            response = urllib.urlopen(url)
+            data = json.loads(response.read())
+        except Exception as out:
+            logging.debug("Error or no data from API")
+            return { 'consumers': 0, 'depth': 0 }
+
+        self.cache[cache_name] = {'timestamp': time.time(), 'consumers': int(data['consumers']),
+                                    'depth': int(data['messages_ready'])}
+        logging.debug('Rabbit: Caching and returning values for "%s": consumers: %i, depth: %i' %
+                      ( cache_name,
+                        self.cache[cache_name]['consumers'],
+                        self.cache[cache_name]['depth']))
+        return self.cache[cache_name]
+

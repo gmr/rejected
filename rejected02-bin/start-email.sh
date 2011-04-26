971dcbac9b949ab6a1a7a06287aaa1586d74333f
Gavin M. Roy <gmr@myyearbook.com>
2011-04-26 18:25:48 -0400

    Update emails tart script

--- rejected02-bin/start-email.sh
+++ rejected02-bin/start-email.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-sudo su rejected -c "/opt/rejected/bin/rejected.py -c /opt/rejected/email_spooler/etc/yahoo.yaml -d"
-sudo su rejected -c "/opt/rejected/bin/rejected.py -c /opt/rejected/email_spooler/etc/yahoo.yaml -d"
-sudo su rejected -c "/opt/rejected/bin/rejected.py -c /opt/rejected/email_spooler/etc/yahoo.yaml -d"
-sudo su rejected -c "/opt/rejected/bin/rejected.py -c /opt/rejected/email_spooler/etc/yahoo.yaml -d"
 sudo su rejected -c "/opt/rejected/bin/rejected.py -c /opt/rejected/email_composer/etc/forgot_password.yaml -d"
+sudo su rejected -c "/opt/rejected/bin/rejected.py -c /opt/rejected/email_composer/etc/match_iphone.yaml -d"

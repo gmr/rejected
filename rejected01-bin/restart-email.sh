7aa06446747c73adb2dbcbe12ea17044ba3e84c0
Gavin M. Roy <gmr@myyearbook.com>
2011-05-03 18:14:55 -0400

    kill -9

--- rejected01-bin/restart-email.sh
+++ rejected01-bin/restart-email.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ps aux |grep '[e]mail_spooler' | awk '{print $2}' | sudo xargs kill
+ps aux |grep '[e]mail_spooler' | awk '{print $2}' | sudo xargs kill -9
 /opt/rejected/bin/opslog.py 'Restart email_spooler rejected consumers'
 /opt/rejected/bin/start-email.sh

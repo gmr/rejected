541a5b022352ca387ef8eeb07831978cfe5eb8b1
Joe Hansche <jhansche@myyearbook.com>
2011-05-10 15:27:31 -0400

    Allow running consumers in foreground

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# Usage: start.sh [ <consumer_name> [-v] ]
+#
+# If starting a single consumer, you can pass -v as the 2nd argument
+# to run in the foreground
+
+# By default, run in the background (-d = detached)
+FGBG=${2:--d}
+
 function get_count
 {
   COUNT_FILE=${1//.yaml/.qty}
@@ -12,7 +21,7 @@ function start_consumers
   CONSUMERS=$(get_count $1)
   echo "Starting $CONSUMERS $CONSUMER consumer(s)"
   for x in $(seq 1 $CONSUMERS); do
-    sudo su rejected -c "/opt/rejected/bin/rejected.py -c $1 -d"
+    sudo su rejected -c "/opt/rejected/bin/rejected.py -c $1 ${FGBG}"
   done
   /opt/rejected/bin/opslog.py 'Started $CONSUMERS $CONSUMER rejected consumer(s)'
 }

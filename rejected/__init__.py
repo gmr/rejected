37fa646f4ddafd4db36e5c29b738408f0e26346f
Gavin M. Roy <gmr@myyearbook.com>
2011-07-23 21:54:51 -0400

    Fix the variable name for since and update the version number

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -3,7 +3,7 @@ Rejected, a Python AMQP Consumer daemon
 
 """
 __author__ = 'Gavin M. Roy <gmr@myyearbook.com>'
-__date__    = "2009-09-10"
-__version__ = "1.0.0"
+__since__    = "2009-09-10"
+__version__ = "2.0.1"
 
 __all__ = ['cli', 'common', 'mcp', 'utils']

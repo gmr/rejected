f5a30b262c5bcde159e59969687483674f9d2ae5
Gavin M. Roy <gmr@myyearbook.com>
2011-09-27 15:01:21 -0400

    Bugfix for the Consumer -> RejectedConsumer revision bump

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -4,6 +4,6 @@ Rejected, a Python AMQP Consumer daemon
 """
 __author__ = 'Gavin M. Roy <gmr@myyearbook.com>'
 __since__    = "2009-09-10"
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 
 __all__ = ['cli', 'common', 'consumer', 'mcp', 'utils']

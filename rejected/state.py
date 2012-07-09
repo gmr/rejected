a6524de5babcfed3f70a9158b490615a6ea1921a
Gavin M. Roy <gmr@meetme.com>
2012-07-09 18:12:22 -0400

    Base state class and tests

---
+++ rejected/state.py
@@ -0,0 +1,106 @@
+"""Base object that carries runtime state info"""
+import logging
+import time
+
+logger = logging.getLogger(__name__)
+
+
+class State(object):
+    """Class that is to be extended by MCP and process for maintaining the
+    internal state of the application.
+
+    """
+    # State constants
+    STATE_INITIALIZING = 0x01
+    STATE_CONNECTING = 0x02
+    STATE_IDLE = 0x03
+    STATE_ACTIVE = 0x04
+    STATE_STOP_REQUESTED = 0x05
+    STATE_SHUTTING_DOWN = 0x06
+    STATE_STOPPED = 0x07
+
+    # For reverse lookup
+    _STATES = {0x01: 'Initializing',
+               0x02: 'Connecting',
+               0x03: 'Idle',
+               0x04: 'Active',
+               0x05: 'Stop Requested',
+               0x06: 'Shutting down',
+               0x07: 'Stopped'}
+
+    def __init__(self):
+        """Initialize the state of the object"""
+        self._state = self.STATE_INITIALIZING
+        self._state_start = time.time()
+
+    def _set_state(self, new_state):
+        """Assign the specified state to this consumer object.
+
+        :param int new_state: The new state of the object
+        :raises: ValueError
+
+        """
+        # Make sure it's a valid state
+        if new_state not in self._STATES:
+            raise ValueError('Invalid state value: %r' % new_state)
+
+        # Set the state
+        logger.debug('State changing from %s to %s',
+                     self._STATES[self._state], self._STATES[new_state])
+        self._state = new_state
+        self._state_start = time.time()
+
+    @property
+    def is_connecting(self):
+        """Returns a bool specifying if the consumer is currently connecting.
+
+        :rtype: bool
+
+        """
+        return self._state == self.STATE_CONNECTING
+
+    @property
+    def is_idle(self):
+        """Returns a bool specifying if the consumer is currently idle.
+
+        :rtype: bool
+
+        """
+        return self._state == self.STATE_IDLE
+
+    @property
+    def is_running(self):
+        """Returns a bool determining if the consumer is in a running state or
+        not
+
+        :rtype: bool
+
+        """
+        return self._state in [self.STATE_IDLE, self.STATE_ACTIVE]
+
+    @property
+    def is_shutting_down(self):
+        """Designates if the consumer is shutting down.
+
+        :rtype: bool
+
+        """
+        return self._state == self.STATE_SHUTTING_DOWN
+
+    @property
+    def is_stopped(self):
+        """Returns a bool determining if the consumer is stopped or stopping
+
+        :rtype: bool
+
+        """
+        return self._state in [self.STATE_SHUTTING_DOWN, self.STATE_STOPPED]
+
+    @property
+    def state_description(self):
+        """Return the string description of our running state.
+
+        :rtype: str
+
+        """
+        return self._STATES[self._state]
---
+++ tests/test_state.py
@@ -0,0 +1,172 @@
+"""Tests for the State Class"""
+import mock
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from rejected import state
+
+
+class TestState(unittest.TestCase):
+
+    def setUp(self):
+        self._obj = state.State()
+
+    def test_set_state_invalid_value(self):
+        self.assertRaises(ValueError, self._obj._set_state, 9999)
+
+    def test_set_state_expected_assignment(self):
+        self._state = self._obj.STATE_IDLE
+        self._obj._set_state(self._obj.STATE_CONNECTING)
+        self.assertEqual(self._obj._state, self._obj.STATE_CONNECTING)
+
+    def test_set_state_state_start(self):
+        self._state = self._obj.STATE_IDLE
+        value = 86400
+        with mock.patch('time.time', return_value=value):
+            self._obj._set_state(self._obj.STATE_CONNECTING)
+            self.assertEqual(self._obj._state_start, value)
+
+    def test_state_initializing_desc(self):
+        self._obj._state = self._obj.STATE_INITIALIZING
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_INITIALIZING])
+
+    def test_state_connecting_desc(self):
+        self._obj._state = self._obj.STATE_CONNECTING
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_CONNECTING])
+
+    def test_state_idle_desc(self):
+        self._obj._state = self._obj.STATE_IDLE
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_IDLE])
+
+    def test_state_active_desc(self):
+        self._obj._state = self._obj.STATE_ACTIVE
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_ACTIVE])
+
+    def test_state_stop_requested_desc(self):
+        self._obj._state = self._obj.STATE_STOP_REQUESTED
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_STOP_REQUESTED])
+
+    def test_state_shutting_down_desc(self):
+        self._obj._state = self._obj.STATE_SHUTTING_DOWN
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_SHUTTING_DOWN])
+
+    def test_state_stopped_desc(self):
+        self._obj._state = self._obj.STATE_STOPPED
+        self.assertEqual(self._obj.state_description,
+                         self._obj._STATES[self._obj.STATE_STOPPED])
+
+    def test_is_idle_state_initializing(self):
+        self._obj._state = self._obj.STATE_INITIALIZING
+        self.assertFalse(self._obj.is_idle)
+
+    def test_is_idle_state_connecting(self):
+        self._obj._state = self._obj.STATE_CONNECTING
+        self.assertFalse(self._obj.is_idle)
+
+    def test_is_idle_state_idle(self):
+        self._obj._state = self._obj.STATE_IDLE
+        self.assertTrue(self._obj.is_idle)
+
+    def test_is_idle_state_processing(self):
+        self._obj._state = self._obj.STATE_ACTIVE
+        self.assertFalse(self._obj.is_idle)
+
+    def test_is_idle_state_stop_requested(self):
+        self._obj._state = self._obj.STATE_STOP_REQUESTED
+        self.assertFalse(self._obj.is_idle)
+
+    def test_is_idle_state_shutting_down(self):
+        self._obj._state = self._obj.STATE_SHUTTING_DOWN
+        self.assertFalse(self._obj.is_idle)
+
+    def test_is_idle_state_stopped(self):
+        self._obj._state = self._obj.STATE_STOPPED
+        self.assertFalse(self._obj.is_idle)
+
+    def test_is_running_state_initializing(self):
+        self._obj._state = self._obj.STATE_INITIALIZING
+        self.assertFalse(self._obj.is_running)
+
+    def test_is_running_state_connecting(self):
+        self._obj._state = self._obj.STATE_CONNECTING
+        self.assertFalse(self._obj.is_running)
+
+    def test_is_running_state_idle(self):
+        self._obj._state = self._obj.STATE_IDLE
+        self.assertTrue(self._obj.is_running)
+
+    def test_is_running_state_processing(self):
+        self._obj._state = self._obj.STATE_ACTIVE
+        self.assertTrue(self._obj.is_running)
+
+    def test_is_running_state_stop_requested(self):
+        self._obj._state = self._obj.STATE_STOP_REQUESTED
+        self.assertFalse(self._obj.is_running)
+
+    def test_is_running_state_shutting_down(self):
+        self._obj._state = self._obj.STATE_SHUTTING_DOWN
+        self.assertFalse(self._obj.is_running)
+
+    def test_is_running_state_stopped(self):
+        self._obj._state = self._obj.STATE_STOPPED
+        self.assertFalse(self._obj.is_running)
+
+    def test_is_shutting_down_state_initializing(self):
+        self._obj._state = self._obj.STATE_INITIALIZING
+        self.assertFalse(self._obj.is_shutting_down)
+
+    def test_is_shutting_down_state_connecting(self):
+        self._obj._state = self._obj.STATE_CONNECTING
+        self.assertFalse(self._obj.is_shutting_down)
+
+    def test_is_shutting_down_state_idle(self):
+        self._obj._state = self._obj.STATE_IDLE
+        self.assertFalse(self._obj.is_shutting_down)
+
+    def test_is_shutting_down_state_processing(self):
+        self._obj._state = self._obj.STATE_ACTIVE
+        self.assertFalse(self._obj.is_shutting_down)
+
+    def test_is_shutting_down_state_stop_requested(self):
+        self._obj._state = self._obj.STATE_STOP_REQUESTED
+        self.assertFalse(self._obj.is_shutting_down)
+
+    def test_is_shutting_down_state_shutting_down(self):
+        self._obj._state = self._obj.STATE_SHUTTING_DOWN
+        self.assertTrue(self._obj.is_shutting_down)
+
+    def test_is_shutting_down_state_stopped(self):
+        self._obj._state = self._obj.STATE_STOPPED
+        self.assertFalse(self._obj.is_shutting_down)
+
+    def test_is_stopped_state_initializing(self):
+        self._obj._state = self._obj.STATE_INITIALIZING
+        self.assertFalse(self._obj.is_stopped)
+
+    def test_is_stopped_state_connecting(self):
+        self._obj._state = self._obj.STATE_CONNECTING
+        self.assertFalse(self._obj.is_stopped)
+
+    def test_is_stopped_state_idle(self):
+        self._obj._state = self._obj.STATE_IDLE
+        self.assertFalse(self._obj.is_stopped)
+
+    def test_is_stopped_state_processing(self):
+        self._obj._state = self._obj.STATE_ACTIVE
+        self.assertFalse(self._obj.is_stopped)
+
+    def test_is_stopped_state_stop_requested(self):
+        self._obj._state = self._obj.STATE_STOP_REQUESTED
+        self.assertFalse(self._obj.is_stopped)
+
+    def test_is_stopped_state_shutting_down(self):
+        self._obj._state = self._obj.STATE_SHUTTING_DOWN
+        self.assertTrue(self._obj.is_stopped)

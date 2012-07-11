1551c9f456f4ebbe297f45cdd0ead6c66ca6e46b
Gavin M. Roy <gmr@meetme.com>
2012-07-11 01:42:17 -0400

    Allow for an empty, new Properties object to be created
    Default an empty Properties object to set the time and uuid

--- rejected/data.py
+++ rejected/data.py
@@ -3,6 +3,8 @@ Rejected data objects
 
 """
 import copy
+import time
+import uuid
 
 
 class DataObject(object):
@@ -61,24 +63,40 @@ class Properties(DataObject):
 
     """
 
-    def __init__(self, header):
+    def __init__(self, header=None):
         """Create a base object to contain all of the properties we need
 
         :param pika.spec.BasicProperties header: A header object from Pika
 
         """
         DataObject.__init__(self)
-        self.content_type = header.content_type
-        self.content_encoding = header.content_encoding
-        self.headers = copy.deepcopy(header.headers) or dict()
-        self.delivery_mode = header.delivery_mode
-        self.priority = header.priority
-        self.correlation_id = header.correlation_id
-        self.reply_to = header.reply_to
-        self.expiration = header.expiration
-        self.message_id = header.message_id
-        self.timestamp = header.timestamp
-        self.type = header.type
-        self.user_id = header.user_id
-        self.app_id = header.app_id
-        self.cluster_id = header.cluster_id
+        if header:
+            self.app_id = header.app_id
+            self.cluster_id = header.cluster_id
+            self.content_type = header.content_type
+            self.content_encoding = header.content_encoding
+            self.correlation_id = header.correlation_id
+            self.delivery_mode = header.delivery_mode
+            self.expiration = header.expiration
+            self.headers = copy.deepcopy(header.headers) or dict()
+            self.priority = header.priority
+            self.reply_to = header.reply_to
+            self.message_id = header.message_id
+            self.timestamp = header.timestamp
+            self.type = header.type
+            self.user_id = header.user_id
+        else:
+            self.app_id = None
+            self.cluster_id = None
+            self.content_type = 'text/text'
+            self.content_encoding = None
+            self.correlation_id = None
+            self.delivery_mode = 1
+            self.expiration = None
+            self.headers = dict()
+            self.message_id = str(uuid.uuid4())
+            self.priority = None
+            self.reply_to = None
+            self.timestamp = int(time.time())
+            self.type = None
+            self.user_id = None

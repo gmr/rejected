648b07fc34a1e7d250f3e16e74372c615b3dee9a
Gavin M. Roy <gavinr@aweber.com>
2014-12-18 10:10:39 -0500

    Update example

--- rejected/example.py
+++ rejected/example.py
@@ -10,13 +10,13 @@ LOGGER = logging.getLogger(__name__)
 class Consumer(consumer.Consumer):
 
     def process(self):
-        #LOGGER.info('Received message %s, a %s message: %r',
-        #            self.message_id, self.message_type, self.body)
+        LOGGER.debug('Received message %s, a %s message: %r',
+                     self.message_id, self.message_type, self.body)
         chance = random.randint(0, 100)
         if chance < 1:
-            raise consumer.MessageException('zomg')
-        #if 0 < chance < 3:
-        #    raise consumer.MessageException('reject')
-        #elif 2 < chance < 4:
-        #    raise ValueError('Unhandled exception')
+            raise consumer.ConsumerException('zomg')
+        elif 0 < chance < 3:
+            raise consumer.MessageException('reject')
+        elif 2 < chance < 4:
+            raise ValueError('Unhandled exception')
 

0a6ae1149ee73ca0de359030834225b03710433f
Gavin M. Roy <gavinmroy@gmail.com>
2015-04-24 00:36:50 -0400

    MCP Cleanup
    - Is silly to have private vars since this is not a library
    - Change the consumers dict to an object to make it easier to reference
    - No need to check for zombie processes, silly me
    - Clean up how the -o and -q cli switches filter data
    - Fix some overriding of top level import names

--- tests/test_mcp.py
+++ tests/test_mcp.py
@@ -1,28 +1,29 @@
 """Tests for the MCP"""
 import mock
 import multiprocessing
+from mock import patch
 
 from helper import config
 from rejected import mcp
 from . import test_state
 
+
 class TestMCP(test_state.TestState):
 
-    CONFIG = {'poll_interval': 30.0, 'log_stats': True}
+    CONFIG = {'poll_interval': 30.0, 'log_stats': True, 'Consumers': {}}
 
-    @mock.patch.object(multiprocessing, 'Queue')
-    def setUp(self, mock_queue_unused):
-
+    @patch.object(multiprocessing, 'Queue')
+    def setUp(self, _mock_queue_unused):
         self.cfg = config.Config()
         self.cfg.application.update(self.CONFIG)
         self._obj = mcp.MasterControlProgram(self.cfg)
 
     def test_mcp_init_consumers_dict(self):
-        self.assertIsInstance(self._obj._consumers, dict)
+        self.assertIsInstance(self._obj.consumers, dict)
 
     def test_mcp_init_consumers_dict_empty(self):
-        self.assertTrue(not self._obj._consumers, dict)
+        self.assertTrue(not self._obj.consumers, dict)
 
     def test_mcp_init_queue_initialized(self):
-        self.assertIsInstance(self._obj._stats_queue, mock.MagicMock)
+        self.assertIsInstance(self._obj.stats_queue, mock.MagicMock)
 

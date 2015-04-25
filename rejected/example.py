da229ef5c2f8ffa7ab9be6c83325d3c7cf2ee55e
Gavin M. Roy <gavinmroy@gmail.com>
2015-04-24 22:38:43 -0400

    Add async example and config

--- rejected/example.py
+++ rejected/example.py
@@ -3,6 +3,9 @@ from rejected import consumer
 import logging
 import random
 
+from tornado import gen
+from tornado import httpclient
+
 __version__ = '1.0.0'
 
 LOGGER = logging.getLogger(__name__)
@@ -11,8 +14,8 @@ LOGGER = logging.getLogger(__name__)
 class ExampleConsumer(consumer.Consumer):
 
     def process(self):
-        LOGGER.debug('Message: %r', self._message.body)
-        action = random.randint(0, 1000)
+        LOGGER.info('Message: %r', self.body)
+        action = random.randint(0, 100)
         if action == 0:
             raise ValueError('Unhandled exception')
         elif action < 2:
@@ -20,3 +23,13 @@ class ExampleConsumer(consumer.Consumer):
         elif action < 5:
             raise consumer.MessageException('reject')
 
+
+class AsyncExampleConsumer(consumer.Consumer):
+
+    @gen.coroutine
+    def process(self):
+        LOGGER.info('Message: %r', self.body)
+        http_client = httpclient.AsyncHTTPClient()
+        results = yield [http_client.fetch('http://www.github.com'),
+                         http_client.fetch('http://www.reddit.com')]
+        [LOGGER.info('Length: %s' % len(r.body)) for r in results]

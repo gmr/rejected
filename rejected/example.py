fd4e7d53c1e065bb1205cbdf0fd8540f55a4bd92
Gavin M. Roy <gavinmroy@gmail.com>
2015-04-24 22:49:46 -0400

    Better examples

--- rejected/example.py
+++ rejected/example.py
@@ -14,11 +14,9 @@ LOGGER = logging.getLogger(__name__)
 class ExampleConsumer(consumer.Consumer):
 
     def process(self):
-        LOGGER.info('Message: %r', self.body)
-        action = random.randint(0, 100)
+        LOGGER.debug('Message: %r', self.body)
+        action = random.randint(0, 10000)
         if action == 0:
-            raise ValueError('Unhandled exception')
-        elif action < 2:
             raise consumer.ConsumerException('zomg')
         elif action < 5:
             raise consumer.MessageException('reject')
@@ -28,8 +26,8 @@ class AsyncExampleConsumer(consumer.Consumer):
 
     @gen.coroutine
     def process(self):
-        LOGGER.info('Message: %r', self.body)
+        LOGGER.debug('Message: %r', self.body)
         http_client = httpclient.AsyncHTTPClient()
         results = yield [http_client.fetch('http://www.github.com'),
                          http_client.fetch('http://www.reddit.com')]
-        [LOGGER.info('Length: %s' % len(r.body)) for r in results]
+        LOGGER.info('Length: %r', [len(r.body) for r in results])

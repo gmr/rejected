f1be3f0920bbd270a5906364e77182b67ae4c354
Gavin M. Roy <gavinr@aweber.com>
2015-09-16 14:53:44 -0400

    Include a NullHandler to avoid logging warnings

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -6,6 +6,20 @@ __author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
 __since__ = "2009-09-10"
 __version__ = "3.7.0"
 
+import logging
+try:
+    # not available in python 2.6
+    from logging import NullHandler
+except ImportError:
+
+    class NullHandler(logging.Handler):
+
+        def emit(self, record):
+            pass
+
+# Add NullHandler to prevent logging warnings
+logging.getLogger(__name__).addHandler(NullHandler())
+
 from consumer import Consumer
 from consumer import PublishingConsumer
 from consumer import SmartConsumer

eb5288ff8c7b5f7aa2dec90c4200e6f2b6a11ee1
Gavin M. Roy <gavinr@aweber.com>
2015-10-07 14:51:33 -0400

    Include ProcessingException

--- rejected/example.py
+++ rejected/example.py
@@ -9,15 +9,17 @@ from tornado import httpclient
 __version__ = '1.0.0'
 
 
-class ExampleConsumer(consumer.Consumer):
+class ExampleConsumer(consumer.SmartConsumer):
 
     def process(self):
         self.logger.debug('Message: %r', self.body)
-        action = random.randint(0, 10000)
+        action = random.randint(0, 100)
         if action == 0:
             raise consumer.ConsumerException('zomg')
         elif action < 5:
             raise consumer.MessageException('reject')
+        elif action < 10:
+            raise consumer.ProcessingException('publish')
 
 
 class AsyncExampleConsumer(consumer.Consumer):

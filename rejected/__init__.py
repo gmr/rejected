1246690d1844be715a7c5dcea03867c98ee05170
Gavin M. Roy <gavinr@aweber.com>
2015-10-14 20:53:27 -0400

    Bump the version

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -4,7 +4,7 @@ Rejected is a Python RabbitMQ Consumer Framework and Controller Daemon
 """
 __author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
 __since__ = "2009-09-10"
-__version__ = "3.9.3"
+__version__ = "3.9.4"
 
 import logging
 try:

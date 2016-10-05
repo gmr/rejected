c4e0e478b937ce76be30630a7c044ad8f9e4ceb8
Gavin M. Roy <gavinr@aweber.com>
2016-10-05 13:22:23 -0400

    Example Updates

--- examples.py
+++ examples.py
@@ -12,6 +12,7 @@ class ExampleConsumer(consumer.SmartConsumer):
     def process(self):
         self.logger.info('Message: %r', self.body)
         action = random.randint(0, 100)
+        self.stats_incr('action', action)
         if action == 0:
             raise consumer.ConsumerException('zomg')
         elif action < 5:
@@ -26,6 +27,7 @@ class AsyncExampleConsumer(consumer.Consumer):
     def process(self):
         self.logger.info('Message: %r', self.body)
         http_client = httpclient.AsyncHTTPClient()
-        results = yield [http_client.fetch('http://www.google.com'),
-                         http_client.fetch('http://www.bing.com')]
+        with self.stats_track_duration('async_fetch'):
+            results = yield [http_client.fetch('http://www.google.com'),
+                             http_client.fetch('http://www.bing.com')]
         self.logger.info('Length: %r', [len(r.body) for r in results])

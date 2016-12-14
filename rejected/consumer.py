9ce714ee9dbb7aa96917930d09e350a8dbaafe0c
Gavin M. Roy <gavinr@aweber.com>
2016-12-14 14:08:49 -0500

    Update logging and set exception error strings in per-request measurements

--- rejected/consumer.py
+++ rejected/consumer.py
@@ -679,26 +679,31 @@ class Consumer(object):
         except exceptions.ChannelClosed as error:
             self.logger.critical('Channel closed while processing %s: %s',
                                  message_in.delivery_tag, error)
+            self._measurement.set_tag('error', str(error))
             raise gen.Return(None)
 
         except exceptions.ConnectionClosed as error:
             self.logger.critical('Connection closed while processing %s: %s',
                                  message_in.delivery_tag, error)
+            self._measurement.set_tag('error', str(error))
             raise gen.Return(None)
 
         except ConsumerException as error:
-            self.logger.error('ConsumerException processing delivery %s: %r',
+            self.logger.error('ConsumerException processing delivery %s: %s',
                               message_in.delivery_tag, error)
+            self._measurement.set_tag('error', str(error))
             raise gen.Return(data.CONSUMER_EXCEPTION)
 
         except MessageException as error:
-            self.logger.error('MessageException processing delivery %s: %r',
+            self.logger.debug('MessageException processing delivery %s: %s',
                               message_in.delivery_tag, error)
+            self._measurement.set_tag('error', str(error))
             raise gen.Return(data.MESSAGE_EXCEPTION)
 
         except ProcessingException as error:
-            self.logger.error('ProcessingException processing delivery %s: %r',
+            self.logger.debug('ProcessingException processing delivery %s: %s',
                               message_in.delivery_tag, error)
+            self._measurement.set_tag('error', str(error))
             self._republish_processing_error()
             raise gen.Return(data.PROCESSING_EXCEPTION)
 
@@ -710,6 +715,7 @@ class Consumer(object):
             self.log_exception('Exception processing delivery %s: %s',
                                message_in.delivery_tag, error,
                                exc_info=exc_info)
+            self._measurement.set_tag('error', str(error))
             raise gen.Return(data.UNHANDLED_EXCEPTION)
 
         if not self._finished:

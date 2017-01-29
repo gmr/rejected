4f96a388e4cc298b38fafae35f7e3045b17399f7
Gavin M. Roy <gavinr@aweber.com>
2017-01-28 20:30:38 -0500

    Bump version

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -17,7 +17,7 @@ from rejected.consumer import MessageException
 
 __author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
 __since__ = '2009-09-10'
-__version__ = '3.16.7'
+__version__ = '3.17.0'
 
 __all__ = [
     'Consumer',

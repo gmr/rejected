19ce5f04bc866b51534bc3d7a87e9e5e14418f09
Gavin M. Roy <gavinr@aweber.com>
2017-10-09 13:04:58 -0400

    Bump the version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rejected',
-      version='3.18.6',
+      version='3.18.7',
       description='Rejected is a Python RabbitMQ Consumer Framework and '
                   'Controller Daemon',
       long_description=open('README.rst').read(),

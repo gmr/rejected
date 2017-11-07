54726fc5d967919900c37ec6bcd978a82a0641b8
Gavin M. Roy <gavinr@aweber.com>
2017-11-07 17:13:06 -0500

    Add branch coverage

--- tests/test_percentile.py
+++ tests/test_percentile.py
@@ -5,13 +5,15 @@ from rejected import utils
 
 class PercentileTestCase(unittest.TestCase):
 
-    def test_90th_percentile(self):
+    def test_90th(self):
         values = [43, 54, 56, 61, 62, 66, 68, 69, 69, 70, 71, 72, 77, 78,
                   79, 85, 87, 88, 89, 93, 95, 96, 98, 99, 99]
         self.assertEqual(utils.percentile(values, 90), 98)
 
-    def test_50th_percentile(self):
+    def test_50th(self):
         values = [43, 54, 56, 61, 62, 66, 68, 69, 69, 70, 71, 72, 77, 78,
                   79, 85, 87, 88, 89, 93, 95, 96, 98, 99, 99]
         self.assertEqual(utils.percentile(values, 50), 77)
 
+    def test_empty_values(self):
+        self.assertIsNone(utils.percentile([], 50))

fcdcb7bc9373fe0afdced4491cded35723081812
Gavin M. Roy <gavinr@aweber.com>
2017-11-24 16:45:05 -0500

    Reflect the drop of previously deprecated classes
    - Part of removing PublishingConsumer and SmartPublishingConsumer
    - Also remove the NullHandler behavior since it really shouldnt be needed here

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -2,23 +2,15 @@
 Rejected is a Python RabbitMQ Consumer Framework and Controller Daemon
 
 """
-import logging
-from logging import NullHandler
-
-# Add NullHandler to prevent logging warnings
-logging.getLogger(__name__).addHandler(NullHandler())
-
 from rejected.consumer import Consumer
-from rejected.consumer import PublishingConsumer
 from rejected.consumer import SmartConsumer
-from rejected.consumer import SmartPublishingConsumer
 from rejected.consumer import ConsumerException
 from rejected.consumer import MessageException
 from rejected.consumer import ProcessingException
 
 __author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
 __since__ = '2009-09-10'
-__version__ = '3.19.5'
+__version__ = '4.0.0'
 
 __all__ = [
     'Consumer',

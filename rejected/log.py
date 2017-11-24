56830d4732b9f07629d28f2bd4ba46ba868b0e1d
Gavin M. Roy <gavinr@aweber.com>
2017-11-24 16:42:50 -0500

    Cleanup and refactor rejected.log
    Not sure why CorrelationFilter was extending Formatter, but that seems wrong.
    
    - Deprecate ``rejected.log.CorrelationFilter`` in favor of the more simple ``rejected.log.CorrelationIDFilter`` and ``rejected.log.NoCorrelationIDFilter``
    - Rename ``rejected.log.CorrelationAdapter`` to ``rejected.log.CorrelationIDAdapter`` to be more uniform in referencing Correlation ID, keeping the old class as well and note its deprecation
    - Add full test coverage
    - Change ``rejected.log.CorrelationIDAdapter.__init__`` method signature to match the documented ``logging.LoggingAdapter.__init__`` signature.

--- rejected/log.py
+++ rejected/log.py
@@ -3,28 +3,103 @@ Logging Related Things
 
 """
 import logging
+import warnings
 
 
-class CorrelationFilter(logging.Formatter):
-    """Filter records that have a correlation_id"""
-    def __init__(self, exists=None):
-        super(CorrelationFilter, self).__init__()
-        self.exists = exists
+class CorrelationFilter(logging.Filter):
+    """Filter records based upon the presence of a ``correlation_id`` attribute
+    and the ``exists`` setting.
+
+    .. deprecated:: 4.0.0
+
+    """
+    def __init__(self, name='', exists=True):
+        """Returns an instance of the Filter class. If ``name`` is specified,
+        it  names a logger which, together with its children, will have its
+        events  allowed through the filter. If name is the empty string, allows
+        every event.
+
+        The ``exists`` attribute is used to toggle the inclusion or exclusion
+        of records filtered based upon the existence of a ``correlation_id``
+        attribute.
+
+        :param str name: The logger name to apply the filter to
+        :param bool exists: When true, filters records out if a
+            ``correlation_id`` is not present.
+
+        """
+        super(CorrelationFilter, self).__init__(name)
+        self._exists = exists
+        warnings.warn('Deprecated; use CorrelationIDFilter and '
+                      'NoCorrelationID filter instead', DeprecationWarning)
 
     def filter(self, record):
-        if self.exists:
-            return hasattr(record, 'correlation_id')
-        return not hasattr(record, 'correlation_id')
+        """Is the specified record to be logged? Returns zero for no,
+        nonzero for yes. If deemed appropriate, the record may be modified
+        in-place by this method.
+
+        :param logging.LogRecord record: The log record to process
+        :rtype: int
+
+        """
+        if self._exists:
+            return int(getattr(record, 'correlation_id', None) is not None)
+        return int(getattr(record, 'correlation_id', None) is None)
 
 
-class CorrelationAdapter(logging.LoggerAdapter):
+class CorrelationIDFilter(logging.Filter):
+    """Filter records that have a correlation_id
+
+    .. versionadded:: 4.0.0
+
+    """
+    def filter(self, record):
+        """Is the specified record to be logged? Returns zero for no,
+        nonzero for yes. If deemed appropriate, the record may be modified
+        in-place by this method.
+
+        :param logging.LogRecord record: The log record to process
+        :rtype: int
+
+        """
+        return int(getattr(record, 'correlation_id', None) is not None)
+
+
+class NoCorrelationIDFilter(logging.Filter):
+    """Filter records that do not have a correlation_id
+
+    .. versionadded:: 4.0.0
+
+    """
+    def filter(self, record):
+        """Is the specified record to be logged? Returns zero for no,
+        nonzero for yes. If deemed appropriate, the record may be modified
+        in-place by this method.
+
+        :param logging.LogRecord record: The log record to process
+        :rtype: int
+
+        """
+        return int(getattr(record, 'correlation_id', None) is None)
+
+
+class CorrelationIDAdapter(logging.LoggerAdapter):
     """A LoggerAdapter that appends the a correlation ID to the message
     record properties.
 
+    .. versionadded:: 4.0.0
+
     """
-    def __init__(self, logger, consumer):
+    def __init__(self, logger, extra):
+        """Create a new instance of the CorrelationAdapter class.
+
+        :param logging.Logger logger: The logger to adapt
+        :param dict extra: Configuration values to pass in
+
+        """
+        super(CorrelationIDAdapter, self).__init__(logger, extra)
         self.logger = logger
-        self.consumer = consumer
+        self.consumer = extra['consumer']
 
     def process(self, msg, kwargs):
         """Process the logging message and keyword arguments passed in to
@@ -35,6 +110,27 @@ class CorrelationAdapter(logging.LoggerAdapter):
         :rtype: (str, dict)
 
         """
-        kwargs['extra'] = {'correlation_id': self.consumer.correlation_id,
-                           'consumer': self.consumer.name}
+        kwargs['extra'] = {
+            'correlation_id': self.consumer.correlation_id,
+            'consumer': self.consumer.name
+        }
         return msg, kwargs
+
+
+class CorrelationAdapter(CorrelationIDAdapter):
+    """A LoggerAdapter that appends the a correlation ID to the message
+    record properties.
+
+    .. deprecated:: 4.0.0
+
+    """
+    def __init__(self, logger, extra):
+        """Create a new instance of the CorrelationAdapter class.
+
+        :param logging.Logger logger: The logger to adapt
+        :param dict extra: Configuration values to pass in
+
+        """
+        super(CorrelationAdapter, self).__init__(logger, extra)
+        warnings.warn('Deprecated; use CorrelationIDAdapter instead',
+                      DeprecationWarning)
---
+++ tests/test_log.py
@@ -0,0 +1,108 @@
+import logging
+from os import path
+import unittest
+import uuid
+
+import mock
+
+from rejected import consumer, log, testing
+
+
+class FilterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.log_record = logging.LogRecord(
+            'test', logging.INFO, path.realpath(__file__), 13,
+            'Example logged message', [], None, 'setUp')
+
+
+class CorrelationIDFilterTestCase(FilterTestCase):
+
+    def setUp(self):
+        super(CorrelationIDFilterTestCase, self).setUp()
+        self.log_filter = log.CorrelationIDFilter()
+
+    def test_that_filter_passes(self):
+        setattr(self.log_record, 'correlation_id', str(uuid.uuid4()))
+        self.assertEqual(self.log_filter.filter(self.log_record), 1)
+
+    def test_that_filter_does_not_pass(self):
+        self.assertEqual(self.log_filter.filter(self.log_record), 0)
+
+
+class NoCorrelationIDFilterTestCase(FilterTestCase):
+
+    def setUp(self):
+        super(NoCorrelationIDFilterTestCase, self).setUp()
+        self.log_filter = log.NoCorrelationIDFilter()
+
+    def test_that_filter_passes(self):
+        self.assertEqual(self.log_filter.filter(self.log_record), 1)
+
+    def test_that_filter_does_not_pass(self):
+        setattr(self.log_record, 'correlation_id', str(uuid.uuid4()))
+        self.assertEqual(self.log_filter.filter(self.log_record), 0)
+
+
+class CorrelationIDAdapterTestCase(testing.AsyncTestCase):
+
+    def test_that_correlation_adapter_is_assigned(self):
+        self.assertIsInstance(self.consumer.logger, log.CorrelationIDAdapter)
+
+    def test_correlation_adapter_process(self):
+        self.consumer._correlation_id = str(uuid.uuid4())
+        msg, kwargs = self.consumer.logger.process('Test', {})
+        self.assertEqual(msg, 'Test')
+        self.assertDictEqual(
+            kwargs['extra'],
+            {'consumer': self.consumer.name,
+             'correlation_id': self.consumer.correlation_id})
+
+
+# Tests for deprecated classes
+
+
+class CorrelationAdapterTestCase(unittest.TestCase):
+
+    def test_that_warnings_warn_is_invoked(self):
+        with mock.patch('warnings.warn') as warn:
+            _adapter = log.CorrelationAdapter(
+                logging.getLogger(__name__),
+                {'consumer': mock.Mock(spec=consumer.Consumer)})
+            warn.assert_called_once()
+
+
+class CorrelationFilterDeprecationTestCase(unittest.TestCase):
+
+    def test_that_warnings_warn_is_invoked(self):
+        with mock.patch('warnings.warn') as warn:
+            log.CorrelationFilter()
+            warn.assert_called_once()
+
+
+class CorrelationFilterExistsTestCase(FilterTestCase):
+
+    def setUp(self):
+        super(CorrelationFilterExistsTestCase, self).setUp()
+        self.log_filter = log.CorrelationFilter(exists=True)
+
+    def test_that_filter_passes(self):
+        setattr(self.log_record, 'correlation_id', str(uuid.uuid4()))
+        self.assertEqual(self.log_filter.filter(self.log_record), 1)
+
+    def test_that_filter_does_not_pass(self):
+        self.assertEqual(self.log_filter.filter(self.log_record), 0)
+
+
+class CorrelationFilterExistsFalseTestCase(FilterTestCase):
+
+    def setUp(self):
+        super(CorrelationFilterExistsFalseTestCase, self).setUp()
+        self.log_filter = log.CorrelationFilter(exists=False)
+
+    def test_that_filter_passes(self):
+        self.assertEqual(self.log_filter.filter(self.log_record), 1)
+
+    def test_that_filter_does_not_pass(self):
+        setattr(self.log_record, 'correlation_id', str(uuid.uuid4()))
+        self.assertEqual(self.log_filter.filter(self.log_record), 0)

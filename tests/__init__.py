9b500a32426b216447f9df94b4f49f3b97bbbef1
Gavin M. Roy <gavinr@aweber.com>
2017-11-24 16:43:18 -0500

    Automatically import environment variables from build/test-environment

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,13 @@
-__author__ = 'gmr'
+import os
+
+
+def setup_module():
+    try:
+        with open('build/test-environment') as f:
+            for line in f:
+                if line.startswith('export '):
+                    line = line[7:]
+                name, _, value = line.strip().partition('=')
+                os.environ[name] = value
+    except IOError:
+        pass

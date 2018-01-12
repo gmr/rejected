14a33aaeffe7f4c13945fb3c4a08f5c68b692117
Gavin M. Roy <gavinr@aweber.com>
2018-01-12 14:55:00 -0500

    Add a decorator for consumer.Consumer
    Decorate checks for the presence of Consumer._message as a conditional and bypasses the function if _message is not set. This is to reduce code duplication.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 
 from rejected import utils
 
@@ -25,3 +26,51 @@ class TestImportNamspacedClass(unittest.TestCase):
     def test_import_consumer_failure(self):
         self.assertRaises(ImportError, utils.import_consumer,
                           'rejected.fake_module.Classname')
+
+
+class Decorated(object):
+
+    def __init__(self):
+        self._message = {}
+
+    @utils.message_property
+    def foo(self):
+        return self._message['foo']
+
+    def _get_bar(self):
+        return self._message.get('bar')
+
+    bar = utils.message_property(getter=_get_bar)
+
+
+class MessagePropertyDecoratorTestCase(unittest.TestCase):
+
+    def test_empty_foo(self):
+        obj = Decorated()
+        self.assertIsNone(obj.foo)
+
+    def test_set_foo(self):
+        expectation = str(uuid.uuid4())
+        obj = Decorated()
+        obj._message['foo'] = expectation
+        self.assertEqual(obj.foo, expectation)
+
+    def test_foo_assignment(self):
+        obj = Decorated()
+        with self.assertRaises(AttributeError):
+            obj.foo = 'bar'
+
+    def test_empty_bar(self):
+        obj = Decorated()
+        self.assertIsNone(obj.bar)
+
+    def test_set_bar(self):
+        expectation = str(uuid.uuid4())
+        obj = Decorated()
+        obj._message['bar'] = expectation
+        self.assertEqual(obj.bar, expectation)
+
+    def test_bar_assignment(self):
+        obj = Decorated()
+        with self.assertRaises(AttributeError):
+            obj.bar = 'foo'

1e01017a33ea6affa0adec55ed0d7b1a2442f5dd
Gavin M. Roy <gavinr@aweber.com>
2018-01-19 17:17:49 -0500

    Add coverage for all of rejected.errors

---
+++ tests/test_errors.py
@@ -0,0 +1,21 @@
+import unittest
+
+import mock
+
+from rejected import errors
+
+
+class RejectedExceptionTestCase(unittest.TestCase):
+
+    def test_error_repr(self):
+        err = errors.RejectedException('test {}', 'test-metric', 1)
+        self.assertEqual(repr(err), 'RejectedException(test 1)')
+
+
+
+class RabbitMQExceptionTestCase(unittest.TestCase):
+
+    def test_error_repr(self):
+        err = errors.RabbitMQException('conn1', 400, 'Test')
+        self.assertEqual(repr(err),
+                         'RabbitMQException(conn1 Error: (400) Test)')

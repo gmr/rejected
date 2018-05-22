67df1dc457916d069ecf78b15c0740767addbe81
Gavin M. Roy <gavinr@aweber.com>
2018-05-22 17:08:14 -0400

    Add ability to connect to statsd via TCP

--- rejected/statsd.py
+++ rejected/statsd.py
@@ -13,6 +13,8 @@ import logging
 import os
 import socket
 
+from tornado import iostream
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -42,11 +44,71 @@ class Client(object):
         self._address = (self._setting('host', self.DEFAULT_HOST),
                          int(self._setting('port', self.DEFAULT_PORT)))
         self._prefix = self._setting('prefix', self.DEFAULT_PREFIX)
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
-                                     socket.IPPROTO_UDP)
+        self._tcp_sock, self._udp_sock = None, None
+        if self._setting('tcp', 'false').lower() == 'true':
+            self._tcp_sock = self._tcp_socket()
+        else:
+            self._udp_sock = self._udp_socket()
 
-    def stop(self):  # pragma: nocover
-        pass
+    def add_timing(self, key, value=0):
+        """Add a timer value to statsd for the specified key
+
+        :param str key: The key to add the timing to
+        :param int or float value: The value of the timing in seconds
+
+        """
+        return self._send(key, value * 1000, 'ms')
+
+    def incr(self, key, value=1):
+        """Increment the counter value in statsd
+
+        :param str key: The key to increment
+        :param int value: The value to increment by, defaults to 1
+
+        """
+        return self._send(key, value, 'c')
+
+    def set_gauge(self, key, value):
+        """Set a gauge value in statsd
+
+        :param str key: The key to set the value for
+        :param int or float value: The value to set
+
+        """
+        return self._send(key, value, 'g')
+
+    def stop(self):
+        """Close the socket if connected via TCP."""
+        if self._tcp_sock:
+            self._tcp_sock.close()
+
+    def _build_payload(self, key, value, metric_type):
+        """Return the """
+        if self._setting('include_hostname', True):
+            return self.PAYLOAD_HOSTNAME.format(
+                self._prefix, self._hostname, self._consumer_name, key, value,
+                metric_type)
+        return self.PAYLOAD_NO_HOSTNAME.format(
+            self._prefix, self._consumer_name, key, value,
+            metric_type)
+
+    def _send(self, key, value, metric_type):
+        """Send the specified value to the statsd daemon via UDP without a
+        direct socket connection.
+
+        :param str key: The key name to send
+        :param int or float value: The value for the key
+
+        """
+        payload = self._build_payload(key, value, metric_type)
+        LOGGER.debug('Sending statsd payload: %r', payload)
+        try:
+            if self._tcp_sock:
+                return self._tcp_sock.write(payload.encode('utf-8'))
+            else:
+                self._udp_sock.sendto(payload.encode('utf-8'), self._address)
+        except (OSError, socket.error) as error:  # pragma: nocover
+            LOGGER.exception('Error sending statsd metric: %s', error)
 
     def _setting(self, key, default):
         """Return the setting, checking config, then the appropriate
@@ -64,54 +126,37 @@ class Client(object):
             self._settings[key] = value
         return self._settings[key]
 
-    def add_timing(self, key, value=0):
-        """Add a timer value to statsd for the specified key
+    def _tcp_on_closed(self):
+        """Invoked when the socket is closed."""
+        LOGGER.warning('Disconnected from statsd, reconnecting')
+        self._ready = False
+        self._tcp_sock = self._tcp_socket()
 
-        :param str key: The key to add the timing to
-        :param int or float value: The value of the timing in seconds
+    def _tcp_on_connected(self):
+        """Invoked when the IOStream is connected"""
+        LOGGER.debug('Connected to statsd at %s via TCP', self._address)
 
-        """
-        self._send(key, value * 1000, 'ms')
+    def _tcp_socket(self):
+        """Connect to statsd via TCP and return the IOStream handle.
 
-    def incr(self, key, value=1):
-        """Increment the counter value in statsd
-
-        :param str key: The key to increment
-        :param int value: The value to increment by, defaults to 1
-
-        """
-        self._send(key, value, 'c')
-
-    def set_gauge(self, key, value):
-        """Set a gauge value in statsd
-
-        :param str key: The key to set the value for
-        :param int or float value: The value to set
+        :rtype: iostream.IOStream
 
         """
-        self._send(key, value, 'g')
-
-    def _send(self, key, value, metric_type):
-        """Send the specified value to the statsd daemon via UDP without a
-        direct socket connection.
-
-        :param str key: The key name to send
-        :param int or float value: The value for the key
-
-        """
+        sock = iostream.IOStream(socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP))
         try:
-            payload = self._build_payload(key, value, metric_type)
-            LOGGER.debug('Sending statsd payload: %r', payload)
-            self._socket.sendto(payload.encode('utf-8'), self._address)
-        except socket.error:  # pragma: nocover
-            LOGGER.exception('Error sending statsd metric')
+            sock.connect(self._address, self._tcp_on_connected)
+        except (OSError, socket.error) as error:
+            LOGGER.error('Failed to connect via TCP: %s', error)
+        sock.set_close_callback(self._tcp_on_closed)
+        return sock
 
-    def _build_payload(self, key, value, metric_type):
-        """Return the """
-        if self._setting('include_hostname', True):
-            return self.PAYLOAD_HOSTNAME.format(
-                self._prefix, self._hostname, self._consumer_name, key, value,
-                metric_type)
-        return self.PAYLOAD_NO_HOSTNAME.format(
-            self._prefix, self._consumer_name, key, value,
-            metric_type)
+    @staticmethod
+    def _udp_socket():
+        """Return the UDP socket handle
+
+        :rtype: socket.socket
+
+        """
+        return socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                             socket.IPPROTO_UDP)
--- tests/test_statsd.py
+++ tests/test_statsd.py
@@ -1,16 +1,17 @@
+import re
 import socket
 import unittest
 import uuid
 
 import mock
+import random
+from tornado import gen, iostream, tcpserver, testing
 
 from rejected import statsd
-
 
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        super(TestCase, self).setUp()
         self.name = str(uuid.uuid4())
         self.settings = self.get_settings()
         self.statsd = statsd.Client(self.name, self.settings)
@@ -20,11 +21,15 @@ class TestCase(unittest.TestCase):
         return {
             'host': '10.1.1.1',
             'port': 8124,
-            'prefix': str(uuid.uuid4())
+            'prefix': str(uuid.uuid4()),
+            'tcp': 'false'
         }
 
     def payload_format(self, key, value, metric_type):
         return self.statsd._build_payload(key, value, metric_type)
+
+
+class UDPTestCase(TestCase):
 
     def test_address(self):
         self.assertEqual(self.statsd._address,
@@ -42,11 +47,12 @@ class TestCase(unittest.TestCase):
                              self.settings[key])
 
 
-class SendTestCase(TestCase):
+class UDPSendTestCase(TestCase):
+
     def setUp(self):
-        super(SendTestCase, self).setUp()
+        super(UDPSendTestCase, self).setUp()
         self.socket = mock.Mock()
-        self.statsd._socket = self.socket
+        self.statsd._udp_sock = self.socket
 
     def test_hostname_in_metric(self):
         self.statsd.add_timing('foo', 2.5)
@@ -87,3 +93,83 @@ class NoHostnameTestCase(TestCase):
         self.statsd.add_timing('foo', 2.5)
         value = self.payload_format('foo', 2500.0, 'ms')
         self.assertNotIn(socket.gethostname().split('.')[0], value)
+
+
+class StatsdServer(tcpserver.TCPServer):
+
+    PATTERN = '[a-z0-9._-]+:[0-9.]+\|(?:g|c|ms)'
+
+    def __init__(self, ssl_options=None, max_buffer_size=None,
+                 read_chunk_size=None):
+        self.packets = []
+        self.reconnect_receive = False
+        super(StatsdServer, self).__init__(
+            ssl_options, max_buffer_size, read_chunk_size)
+
+    def handle_stream(self, stream, address):
+
+        def read_callback(future):
+            result = future.result()
+            self.packets.append(result)
+            if 'reconnect' in result:
+                self.reconnect_receive = True
+                stream.close()
+                return
+            inner_future = stream.read_until_regex(self.PATTERN)
+            self.io_loop.add_future(inner_future, read_callback)
+
+        future = stream.read_until_regex(self.PATTERN)
+        self.io_loop.add_future(future, read_callback)
+
+
+class TCPTestCase(testing.AsyncTestCase):
+
+    def setUp(self):
+        super(TCPTestCase, self).setUp()
+        self.sock, self.statsd_port = testing.bind_unused_port()
+        self.name = str(uuid.uuid4())
+        self.settings = self.get_settings()
+        self.statsd = statsd.Client(self.name, self.settings)
+        self.server = StatsdServer()
+        self.server.add_socket(self.sock)
+
+    def get_settings(self):
+        return {
+            'host': 'localhost',
+            'port': self.statsd_port,
+            'prefix': str(uuid.uuid4()),
+            'tcp': 'true'
+        }
+
+    def payload_format(self, key, value, metric_type):
+        return self.statsd._build_payload(key, value, metric_type)
+
+    @testing.gen_test
+    def test_add_timing(self):
+        self.statsd.add_timing('foo', 2.5)
+        yield gen.sleep(0.1)
+        self.assertIn(self.payload_format('foo', 2500.0, 'ms'), self.server.packets)
+
+    @testing.gen_test
+    def test_incr(self):
+        self.statsd.incr('bar', 2)
+        yield gen.sleep(0.1)
+        self.assertIn(self.payload_format('bar', 2, 'c'), self.server.packets)
+
+    @testing.gen_test
+    def test_set_gauge(self):
+        self.statsd.set_gauge('baz', 98.5)
+        yield gen.sleep(0.1)
+        self.assertIn(self.payload_format('baz', 98.5, 'g'), self.server.packets)
+
+    @testing.gen_test
+    def test_reconnect(self):
+        self.statsd.set_gauge('baz', 98.5)
+        yield gen.sleep(0.1)
+        self.statsd.set_gauge('reconnect', 100)
+        yield gen.sleep(0.1)
+        self.statsd.set_gauge('bar', 10)
+        yield gen.sleep(0.1)
+        self.assertIn(self.payload_format('baz', 98.5, 'g'), self.server.packets)
+        self.assertIn(self.payload_format('reconnect', 100, 'g'), self.server.packets)
+        self.assertIn(self.payload_format('bar', 10, 'g'), self.server.packets)

83563f126af9d708128d9282615eb9dfd66c0763
Gavin M. Roy <gavinr@aweber.com>
2024-01-30 12:12:10 -0500

    Pre-commit testing

--- rejected/__init__.py
+++ rejected/__init__.py
@@ -7,8 +7,4 @@ __author__ = 'Gavin M. Roy <gavinmroy@gmail.com>'
 __since__ = '2009-09-10'
 __version__ = '4.0.0a0'
 
-__all__ = [
-    '__author__',
-    '__since__',
-    '__version__'
-]
+__all__ = ['__author__', '__since__', '__version__']

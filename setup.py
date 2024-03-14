a2c46296def9c558d2a5bdae7d1aebbc7de9ad5b
Gavin M. Roy <gavinr@aweber.com>
2024-03-14 12:53:57 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read_requirements(name):
 
 setuptools.setup(
     name='rejected',
-    version='3.22.4',
+    version='3.23.0',
     description='Rejected is a Python RabbitMQ Consumer Framework and '
                 'Controller Daemon',
     long_description=open('README.rst').read(),
